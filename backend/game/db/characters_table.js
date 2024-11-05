c75e7c27dc1620b28476cfee2d8338282def67e9
SYEONIH <syeonih@naver.com>
2024-11-05 17:25:09 +0900

    sequelize 형식 바꾸기 characters, memberCharacters

---
+++ backend/game/db/characters_table.js
@@ -0,0 +1,36 @@
+const sequelize = require('../../config/db');
+const { DataTypes } = require('sequelize');
+
+const Characters = sequelize.define('Characters', {
+  character_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.BLOB,
+  },
+}, {
+  tableName: 'characters',
+  timestamps: false,
+});
+
+const createCharactersTableAndInsertData = async () => {
+  try {
+    await sequelize.sync();
+    await Characters.bulkCreate([
+      { name: 'Character1', image: null },
+      { name: 'Character2', image: null },
+      { name: 'Character3', image: null },
+    ]);
+    console.log('Characters 테이블 초기화 및 데이터 삽입 성공');
+  } catch (err) {
+    console.error('오류 발생: ', err);
+  }
+};
+
+module.exports = { Characters, createCharactersTableAndInsertData };
