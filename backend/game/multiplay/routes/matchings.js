6583193a9a02381769573eeb8f65625df8f65c72
sunninz <tnals00718@gmail.com>
2024-11-11 15:51:53 +0900

    fix: 매칭 수정

--- backend/game/multiplay/routes/matchings.js
+++ backend/game/multiplay/routes/matchings.js
@@ -6,9 +6,5 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-//매칭 성공시 이동하는 room경로
-router.get('/room/:roomName',(req,res)=>{
-  const roomName = req.params.roomName;
-  res.render("room", { roomName }); 
-});
+
 module.exports = router;
